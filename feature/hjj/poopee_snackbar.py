2746918d9ac3ebf9044cfd06be929524bc97dcd8
전형준 <chariot0720@gmail.com>
2020-10-27 15:07:43 +0900

    Modified 'poopee_snackbar.py'
    Modify code globally. A bluetooth connection error occurs in the thread. It must be fixed.

--- feature/hjj/poopee_snackbar.py
+++ feature/hjj/poopee_snackbar.py
@@ -4,19 +4,39 @@ import threading
 from bluetooth import *
 from time import sleep
 
-FOOD = []
+"""stack for feeding"""
+FEEDING = []
 
-def handle_client(client_socket, bluetooth_socket):
+"""receive a feeding signal from other python files via socket"""
+def handle_client(client_socket):
     try:
         while True:
+            print('Waiting for a feeding signal...')
             data = client_socket.recv(4)
             data = data.decode('utf-8')
             print('Received ' + data)
             for _ in range(int(data)):
-                FOOD.append('on')
-            print(len(FOOD))
+                FEEDING.append('on') # push on stack
     except:
         print('Socket closed!')
+
+"""connect bluetooth and send a feeding signal to the poopee snack bar"""
+def connect_bluetooth(mac_address):
+    while True: # loop for bluetooth connect
+        try:
+            bluetooth_socket = BluetoothSocket(RFCOMM)
+            bluetooth_socket.connect((mac_address, 1))
+            print('Bluetooth connected!')
+            while True: # loop for feeding when bluetooth is connected
+                temp_len = len(FEEDING)
+                print('Number of remaining feeding: ' + temp_len)
+                if temp_len > 0:
+                    bluetooth_socket.send(FEEDING.pop()) # pop on stack
+                    sleep(3)
+                sleep(1)
+        except:
+            print("Bluetooth not connected... retry...")
+            sleep(1)
 
 """read a json file when 'poopee_polling.py' file does not write a json file"""
 def read_json(file_path):
@@ -30,11 +50,14 @@ def read_json(file_path):
             print('Fail to read json file!')
 
 def main():
-    json_path = 'poopee_data.json'
+    # """for bluetooth"""
+    # json_path = 'poopee_data.json'
+    # json_data = read_json(json_path)
 
-    json_data = read_json(json_path)
-    mac_address = json_data['mac_address']
+    # bt_thread = threading.Thread(target=connect_bluetooth, args=(json_data['mac_address'], ))
+    # bt_thread.start() 
 
+    """for socket communication"""
     HOST = '127.0.0.1'
     PORT = 9999
 
@@ -42,27 +65,16 @@ def main():
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen()
-    print("Poopee snack bar server start!")
+    print('Poopee snack bar server start!')
 
     while True:
-        try:
-            bluetooth_socket = BluetoothSocket(RFCOMM)
-            bluetooth_socket.connect((mac_address, 1))
-            print("Bluetooth connected!")
-            while True:
-                print('waiting')
-                client_socket, _ = server_socket.accept()
-                t = threading.Thread(target=handle_client, args=(client_socket, bluetooth_socket, ))
-                t.start()
-                # bluetooth_socket.send("on")
-                # sleep(3)
-        except KeyboardInterrupt:
-            server_socket.close()
-            bluetooth_socket.close()
-            return _
-        except:
-            print("Bluetooth not connected... retry...")
-            sleep(1)        
+        print('Waiting for socket communication...')
+        client_socket, _ = server_socket.accept()
+        t = threading.Thread(target=handle_client, args=(client_socket, ))
+        t.start()
+    
+    server_socket.close()
+                   
 
 if __name__ == '__main__':
     main()
